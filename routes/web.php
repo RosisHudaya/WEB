b4da75203ae628d5ee854b84139941029e58446a
RosisHudaya <rosishudayaputra02@gmail.com>
2022-12-13 23:41:57 +0700

    commit commit

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,8 @@ Route::get('/', function () {
 });
 
 Route::get('/mysql', function () {
-    Artisan::call('migrate:fresh', ['--force' => true]);
+    Artisan::call('rollback', ['--force' => true]);
+    Artisan::call('migrate', ['--force' => true]);
     Artisan::call('db:seed', ['--force' => true]);
 });
 

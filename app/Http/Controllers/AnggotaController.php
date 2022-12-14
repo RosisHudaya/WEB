8ecec4c059e777d9c4c0b429a109fa6a15b50f35
RosisHudaya <rosishudayaputra02@gmail.com>
2022-12-15 05:53:03 +0700

    commit

--- app/Http/Controllers/AnggotaController.php
+++ app/Http/Controllers/AnggotaController.php
@@ -75,7 +75,7 @@ class AnggotaController extends Controller
             //filename to store
             $filenametostore = $filename . '_' . uniqid() . '.' . $extension;
 
-            // Storage::put('public/uploads/' . $filenametostore, fopen($request->file('foto'), 'r+'));
+            Storage::put('public/uploads/' . $filenametostore, fopen($request->file('foto'), 'r+'));
 
             $filepath = storage_path('app/public/uploads/' . $filenametostore);
 
